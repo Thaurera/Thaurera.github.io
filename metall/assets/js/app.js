f2ab5b21b982fefc76dbbf7f5556f869c52a5d91
Thaurera <63438207+Thaurera@users.noreply.github.com>
2020-07-21 10:53:55 +0700

    Add files via upload

---
+++ metall/assets/js/app.js
@@ -0,0 +1,10 @@
+//   Menu Burger
+
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    })
+});
+
+//   ///Menu Burger
